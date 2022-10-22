9cc6e24fe8db0e333982ac3a2cbefe7a463ec1c2
rkolovanov <rodion.kolovanov@inbox.ru>
2022-10-22 04:05:41 +0300

    Add auxiliary-cpp library

--- cmake/Libraries.cmake
+++ cmake/Libraries.cmake
@@ -3,6 +3,7 @@ include_directories(${PROJECT_SOURCE_DIR})
 
 # Find libraries
 find_package(cmake-build-system 0.0.2 REQUIRED)
+find_package(auxiliary-cpp 1.0.0 REQUIRED)
 find_package(SFML 2.5.1 COMPONENTS system graphics window REQUIRED)
 
 if(WITH_TESTS)
